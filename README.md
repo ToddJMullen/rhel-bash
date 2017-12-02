ab7a7ab142a8354058c8a9e22e3463638b316ee2
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-12-02 16:46:53 -0500

    Add README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+This is currently just a catch-all folder for files created while working though various courses & tutorials for: RHEL, BASH, Vim, etc. Or, more generally, command line Linux related exercises.
--- conditionalSamples.sh
+++ conditionalSamples.sh
@@ -6,10 +6,10 @@ then
  echo "\nThere were no arguments passed into the script.\n"
  echo "\nUsage: $(basename $0) <arg1> <arg2> ... <argn>"
  # $0 refers to the script name
- exit 1 #anything non-zero is considered an error exit
+ exit 1 #anything non-zero is considered an error bt convention
 else
  echo "\nYou typed in: $@\n Or, '$*'"
  # "$@" is the special var to list all the positional params entered
- # Also, "$*" holds all the input positional vars as a single value
- # Eg $* = "arg1 arg2 arg3" $@ = "arg1" "arg2" "arg3"
+ # "$*" holds all the input positional vars as a single value
+ # Eg $* => "arg1 arg2 arg3" $@ => "arg1" "arg2" "arg3"
 fi
