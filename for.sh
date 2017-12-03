74868c64617f18a76290ce9774f3405f6518e4bd
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-12-02 22:22:36 -0500

    for.sh, Add more interesting content with sleep delays

--- for.sh
+++ for.sh
@@ -2,8 +2,14 @@
 
 #for loops
 
-for i in 6 5 4 3 2 1 "time's up" ; do
+echo "Count down sequence"
+sleep 2
+
+for i in 6 5 4 3 2 1 "Time's up. Beginning reality check." ; do
 	echo -e "$i\n"
+	sleep 1
 done
+
+echo "All Bitcoin have been erased by EMP!"
 
 
