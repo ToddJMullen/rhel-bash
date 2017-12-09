33cfe8e28cc111fd5030e5acd0de76a53efd0570
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-12-09 16:07:57 -0500

    Edit file names to be more descriptive. - Add formatting in cliInputParamVarsShift.sh

---
+++ cliInputParamVarsShift.sh
@@ -0,0 +1,19 @@
+#1/bin/bash
+#This is a second script so it's not as long
+name=$USER
+shortname=$(cat /etc/passwd | grep "$name" | awk -F : '{print $5}' | cut -d " " -f1)
+echo -e "Hello me: $name (local script var from system var \$USER)\n"
+echo "I also go by $shortname (script var)"
+echo -e "\n\nThis line has posiitional params/args from the CLI (if entered):
+ num 1: $1,
+ second thing: $2,
+ third: $3"
+shift
+echo "Shifting... (shift plucks 0th [=$1] input & 'shifts' all down by one)"
+echo -e "\n\nThis line has the same posiitional params/args from the CLI (after shift):
+ num 1: $1,
+ second thing: $2,
+ third: $3"
+echo "\$# hold the number of pos args: $#
+\$@ Holds all the current args as a list: $@
+\$* Has the current args as a single value: $*"
---
+++ script5.2-Menus.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#creating a simple menu
+
+promptUser()
+{
+trap "" 1 2 3 18 #trap interrupts so user can't exit
+
+while [ "$answer" != "Option3" ]; do
+	read -p "That wasn't an Option!" answer
+	if [ "$answer" = "Option3" ] ; then
+		tput clear
+		echo "Yay!"
+		answer=
+		return
+	else
+		echo "wrong..."
+	fi
+done
+}
+
+
+options="Item1 Item2 Option3"
+
+echo -e "Pick an option or else!\n"
+
+PS3="Enter your choice: "
+
+select choice in $options; do
+#choice splits the values in the var we give it and listens for user input
+#it stores the user response in $REPLY and uses the value to map it to the list
+	echo -e "\nREPLY: $REPLY"
+	echo -e "\nchoice var: $choice"
+	
+	case $choice in
+		"Item1")
+			echo -e "\nYou chose Item1!"
+		break
+		;;
+		"Item2")
+			echo -e "\nYou chose #2"
+		break
+		;;
+		"Option3")
+			echo -e "\nThat is #3"
+		break
+		;;
+	esac
+done;
